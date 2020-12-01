96c1c07fd526f6edcdc27e86f09f20565fc80c23
Matthew Sage <mattsage@users.noreply.github.com>
2020-12-01 14:28:53 +0000

    Update MrJiggles.py

--- MrJiggles.py
+++ MrJiggles.py
@@ -1,39 +1,39 @@
 #https://pyautogui.readthedocs.io/en/latest/index.html
 import pyautogui
 import time
-
+print("#####################")
 print("Mr Jiggles")
+print("#####################")
 print("+------------+")
-print("|            |")
-print("| +---+      |")
-print("| |   | +--+ |")
-print("| |   | |  | |")
-print("| +---+ +--+ |")
-print("|            |")
-print("| X        X |")
-print("| X       XX |")
-print("| XX      X  |")
-print("|  XXXXXXXX  |")
-print("|            |")
-print("|            |")
-print("+-----X------+")
-print("      X")
-print("      X")
-print("     XX")
-print("     X")
-print("   +---+")
-print("   |   |")
-print("   |   |")
-print("   +---+")
+print("|             |")
+print("| +---+       |")
+print("| |   |  +--+ |")
+print("| |   |  |  | |")
+print("| +---+  +--+ |")
+print("|             |")
+print("| X         X |")
+print("| X        XX |")
+print("| XX       X  |")
+print("|  XXXxXXXXX  |")
+print("|             |")
+print("|             |")
+print("+-----[]------+")
+print("      []")
+print("      [][]")
+print("        []")
+print("        []")
+print("      +----+")
+print("      |    |")
+print("      |[][]|")
+print("      +----+")
 
-print("Jiggly Wiggly!")
+pyautogui.typewrite('Jiggly Wiggly!\n', interval=0.1)  
 #confirm(text='', title='', buttons=['OK', 'Cancel'])
 #set pyautogui.FAILSAFE
 while True: #Infinate Loop
 	pyautogui.move(1,0) #move mouse 1pxl
 	pyautogui.move(-1,0)
         time.sleep(180)
-#	pyautogui.typewrite('Hello world!\n', interval=0.2)  #Enter text
 #	pyautogui.PAUSE = 1.0
 #	pyautogui.keyDown('f15') #Keyboard Press
 #	pyautogui.keyUp('f15') #Keyboard Release
